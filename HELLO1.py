561822c60493280b6d3b7b80ef49ecc28795db26
ym <yuya@ymnoMacBook-Pro.local>
2017-08-17 16:49:09 +0900

    no message

--- HELLO1.py
+++ HELLO1.py
@@ -6,8 +6,15 @@ Created on Thu Aug 17 13:15:28 2017
 @author: yuya
 """
 # app.py
+from flask import Flask,render_template
+app=Flask(__name__)
+
+
 @app.route('/')
 def index():
     #template/index.html  のテンプレートを使う
     #message という変数にHelloと代入した状態でテンプレート内で使う
-    return render_template('index.html',message="Hello")
+    return render_template('index.html',message="AAAA")
+
+if __name__ =="__main__":
+    app.run()
